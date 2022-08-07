afc26488f858786a76270df915a1a8623ed6c32b
marwen <marwenkouidhi@gmail.com>
2022-08-07 17:51:09 +0100

    deploy wave contract

---
+++ scripts/run.ts
@@ -0,0 +1,22 @@
+import { ContractFactory } from "ethers";
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const waveContractFactory: ContractFactory = await ethers.getContractFactory(
+    "WavePortal"
+  );
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0); // exit Node process without error
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+};
+runMain();
