d951b4d75715360fa58b6d26083b5bab92b7082e
marwen <marwenkouidhi@gmail.com>
2022-08-07 19:13:00 +0100

    fix react/no-unescaped-entities violation

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="max-w-lg mx-auto text-center pt-24 grid gap-5">
       <h1 className="text-2xl font-bold ">👋 Hey there!</h1>
       <p className="text-gray-500">
-        I am farza and I worked on self-driving cars so that's pretty cool
+        I am farza and I worked on self-driving cars so that&apos;s pretty cool
         right? Connect your Ethereum wallet and wave at me!
       </p>
       <button className="bg-gray-200 hover:bg-gray-300 anima text-sm text-bold cursor-pointer w-full py-2 rounded-md">
