9632fd29b67a792ac168a8fd3358ce3fa33dfe64
marwen <marwenkouidhi@gmail.com>
2022-08-07 19:52:06 +0100

    change lp description

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
     <div className="max-w-lg mx-auto text-center pt-24 grid gap-5">
       <h1 className="text-2xl font-bold ">👋 Hey there!</h1>
       <p className="text-gray-500">
-        I am farza and I worked on self-driving cars so that&apos;s pretty cool
+        I am Marwen and I worked on self-driving cars so that&apos;s pretty cool
         right? Connect your Ethereum wallet and wave at me!
       </p>
       <button className="bg-gray-200 hover:bg-gray-300 anima text-sm text-bold cursor-pointer w-full py-2 rounded-md">
